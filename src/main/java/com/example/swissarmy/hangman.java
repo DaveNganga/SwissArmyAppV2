c3406eba2abc6d13b2f423ec1d79633a239d72e2
arnoldmark <mk772@cairn.edu>
2023-04-25 10:16:46 -0400

    section1-initiate game

--- src/main/java/com/example/swissarmy/hangman.java
+++ src/main/java/com/example/swissarmy/hangman.java
@@ -18,10 +18,25 @@ public class hangman {
     @FXML
     String toGuessWord = "cactus";
     String guessWord = textField.getText();
+  // checks to see if the word is already a match and the game is won if it is
+    boolean check1 = toGuessWord.equalsIgnoreCase(guessWord);
+    // if the word is not a match a few more steps are undertaken before trying again
+    // convert the strings to arrays and compare them and print out what is similar
+    // in the toGuess word
+    if (check1 = true) {
+        System.out.println("Congratulations you win");
+    } else {
+        count+=1;
+        System.out.println("Please try again");
+        // https://stackoverflow.com/questions/32262059/java-find-element-in-array-using-condition-and-lambda
+        // https://www.digitalocean.com/community/tutorials/java-array-contains-value
+        char[] array1 = toGuessWord.toCharArray();
+        char[] array2 = guessWord.toCharArray();
 
-    
+        
 
 
+    }
 
 
 
