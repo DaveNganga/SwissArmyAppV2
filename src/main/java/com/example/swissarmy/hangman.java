c87ea707d57d34f32bd70b70bc1bb0c5df791db6
arnoldmark <mk772@cairn.edu>
2023-04-30 00:30:32 -0400

    hangman section  2
    code to be tested

--- src/main/java/com/example/swissarmy/hangman.java
+++ src/main/java/com/example/swissarmy/hangman.java
@@ -14,6 +14,9 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
+
+
 public class hangman {
 
     @FXML
@@ -24,37 +27,55 @@ public class hangman {
     private Label counts;
     @FXML
     private Label output;
+    private int count = 0;
 
-    int count = 0;
+    public void compareContent(){
+        String guessWord = textField.getText();
+        // checks to see if the word is already a match and the game is won if it is
+        String toGuessWord = "cactus";
+        boolean check1 = toGuessWord.equalsIgnoreCase(guessWord);
+        // if the word is not a match a few more steps are undertaken before trying again
+        // convert the strings to arrays and compare them and print out what is similar
+        // in the toGuess word
+        if (check1) {
+            System.out.println("Congratulations you win");
+        }
+        else
+        {
+            System.out.println("Please try again");
+            count+=1;
+            System.out.println("display1");
+            char[] array1 = toGuessWord.toCharArray();
+             System.out.println(array1);
+             char[] array2 = guessWord.toCharArray();
+             Set<Character> hiddenWord = new HashSet<Character>(Arrays.asList(array1));
+             Set<Character> checkingWord = new HashSet<Character>(Arrays.asList(array2));
+
+             hiddenWord.retainAll(checkingWord);
+            System.out.println("Intersection: + hiddenWord");
+
+
+
+        }
+        if (count == 7) {
+            break;
+
+    }
+}
 
     @FXML
-    String toGuessWord = "cactus";
+    private void Back(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene main = new Scene(fxmlLoader.load(), 800, 600);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Swiss Army");
+        stage.setScene(main);
+        stage.show();
+    }
 
-    String guessWord = textField.getText();
-  // checks to see if the word is already a match and the game is won if it is
-    boolean check1 = toGuessWord.equalsIgnoreCase(guessWord);
-    // if the word is not a match a few more steps are undertaken before trying again
-    // convert the strings to arrays and compare them and print out what is similar
-    // in the toGuess word
-    if (check1 == true) {
-        System.out.println("Congratulations you win");
-    } else
-
-    {
-        // count+=1;
-        System.out.println("Please try again");
         // https://stackoverflow.com/questions/32262059/java-find-element-in-array-using-condition-and-lambda
         // https://www.digitalocean.com/community/tutorials/java-array-contains-value
     }
-       if (check1 == true)
-
-    {
-        char[] array1 = toGuessWord.toCharArray();
-        System.out.println(array1);
-        char[] array2 = guessWord.toCharArray();
-
-        Set<Character> hiddenWord = new HashSet<Character>(Arrays.asList(array1));
-
 
 
 
